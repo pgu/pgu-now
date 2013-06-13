9d244c78ab1d08310e11eeb989a343d1db3ace69
Pascal Guilcher <guilcher.p@sfeir.com>
2013-06-13 14:49:17 +0200

    ..

--- app/scripts/controllers/dashboardCtrl.js
+++ app/scripts/controllers/dashboardCtrl.js
@@ -1,7 +1,43 @@
 'use strict';
 
-angular.module('pguNowApp').controller('DashboardCtrl', ['$scope', '$http', function ($scope, $http) {
+angular.module('pguNowApp').controller('DashboardCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+
+    var currentDate = new Date();
+    var currentDay = pad(currentDate.getDate())//
+            + '/'                                 //
+            + pad(currentDate.getMonth() + 1)  //
+            + '/'                                 //
+            + currentDate.getFullYear()        //
+        ;
+
+    var currentTime = getTimeFormatted(currentDate);
 
     $scope.title = 'Dashboard';
+    $scope.currentDay = currentDay;
+    $scope.currentTime = currentTime;
 
+    function pad(n) {
+        return n < 10 ? '0' + n : n;
+    }
+
+    function getTimeFormatted(date) {
+        return pad(date.getHours())  //
+            + ':'                       //
+            + pad(date.getMinutes()) //
+            + ':'                       //
+            + pad(date.getSeconds()) //
+            ;
+    }
+
+    function updateCurrentTime() {
+
+        var now = new Date();
+        $scope.currentTime = getTimeFormatted(now);
+
+        $timeout(function () {
+            updateCurrentTime();
+        }, 990);
+    }
+
+    updateCurrentTime();
 }]);
